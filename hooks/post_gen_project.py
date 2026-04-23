6cd45c3b78c3ebf93ed2a4fcf11735357634e9ef
yo-aka-gene <yujiokano@keio.jp>
2026-04-24 06:03:05 +0900

    :wrench::sparkles: add apptainer implementations

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import subprocess
 
 try:
@@ -13,12 +14,22 @@ except Exception:
     pass
 
 print("\n" + "=" * 50)
-print("QookingFever Project Successfully Created!")
+print("QookFast Project Successfully Created!")
 print("=" * 50)
+
+has_apptainer = shutil.which("apptainer") is not None
+
+if not has_apptainer:
+    print("\n  [WARNING] Apptainer is not installed or not in PATH.")
+    print("    QookFast requires Apptainer to ensure reproducibility.")
+    print("    Please install it before running `make setup`.")
+    print("    (e.g., Run: sudo apt update && sudo apt install -y apptainer)")
+    print("-" * 55)
+
 print("\nNext steps:")
 print(f"  1. cd {os.path.basename(os.getcwd())}")
-print("  2. make setup   <-- This will take some time (downloading genomes & indexing)")
-print("  3. Move your .fastq.gz files into the raw_data/ directory")
+print("  2. make setup   <-- This will take some time (downloading & indexing)")
+print("  3. Move your .fastq.gz files into the ./raw_data/ directory")
 print("  4. make run")
-print("\nHappy Qooking!")
-print("=" * 50 + "\n")
+print("\nHappy Qooking with QookFast!")
+print("=" * 55 + "\n")
--- {{cookiecutter.__Project_Slug}}/get_versions.sh
+++ {{cookiecutter.__Project_Slug}}/get_versions.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
+SAMTOOLS_VER=$(samtools --version | head -n 1 | awk '{print $2}')
 FASTP_VER=$(fastp --version 2>&1 | awk '{print $2}')
 STAR_VER=$(STAR --version)
 SUBREAD_VER=$(featureCounts -v 2>&1 | grep "featureCounts" | awk '{print $2}')
-SAMTOOLS_VER=$(samtools --version | head -n 1 | awk '{print $2}')
 
-cat <<EOF > software_versions.yaml
-project: {{ cookiecutter.project_name}}
-timestamp: "$(date +"%Y-%m-%d %H:%M:%S")"
-software_versions:
-  fastp: "${FASTP_VER}"
-  STAR: "${STAR_VER}"
-  subread_featureCounts: "${SUBREAD_VER}"
-  samtools: "${SAMTOOLS_VER}"
-EOF
+
+echo "tools: "
+echo "  samtools: \"$SAMTOOLS_VER\""
+echo "  fastp: \"$FASTP_VER\""
+echo "  STAR: \"$STAR_VER\""
+echo "  subread: \"$SUBREAD_VER\""
